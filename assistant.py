facbfc037feb052135af42b6cff8847765ba3d16
Rellin-Entwicklung <55977451+Rellin-Entwicklung@users.noreply.github.com>
2021-01-22 22:20:57 +0100

    some failures eliminated

--- assistant.py
+++ assistant.py
@@ -6,12 +6,62 @@ import tkinter.font as tkFont
 import xml.etree.ElementTree as ET
 import logging
 
+class param:
+    status = {
+        "project": "",
+        "inverter": "",
+        "user": "PD",
+        "date/time": "",
+        "Inp_BLOCK": "not used",
+        "Inp_RUN": "not used",
+        "Inp_UP": "not used",
+        "Inp_DOWN": "not used",
+        "RATED SPEED": "50 hz",
+        "JOG Speed": "15 hz",
+        "CRAWL Speed": "2 hz",
+        "SPEED 4": "not used",
+        "SPEED 5": "not used",
+        "SPEED 6": "not used",
+        "SPEED 7": "not used",
+        "Input_01": "MPI_01",
+        "Input_02": "MPI_02",
+        "Input_03": "MPI_03",
+        "Input_04": "MPI_04",
+        "Input_05": "MPI_05",
+        "acel_time": "1.5",
+        "decel__time": "1.5",
+        "braking_frequ": "2.5",
+        "braking_time": "2.5",
+        "motor_rated_speed": "1400",
+        "motor_rated_torque": "7.5",
+        "motor_rated_voltage": "3 * 400 V",
+        "motor_rated_current": " 27 A",
+
+    }
+
+
+param.status["project"] = "Test Assistant"
+param.status["user"] = "Alex"
+param.status["inverter"] = "Yaskawa G7"
+param.status["date/time"] = "2021-01-22"
+print("klasse param-status ", param.status)
+print("einzelene Werte: ", param.status["project"])
+#fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+#fontStyle_1 = tkFont.Font(family="Lucida Grande", size=12)
+
+
+
+
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 
+
+def irgendwas():
+    print("irgendwas")
 
 def Wizard():
     global ctr
@@ -41,7 +91,7 @@ def Wizard():
     ActionList = []
     for action in xml_root.iter('comm_item'):
         ActionList.append(str(action.text.strip()))
-    print(ActionList)
+    print("actions in List", ActionList)
 
     params = ["Please start inspection drive up",
               "Please start inspection drive down",
@@ -77,27 +127,28 @@ def Wizard():
                "input_04",
                "input_05"]
 
+
     #fontStyle = tkFont.Font(family="Lucida Grande", size=20)
     #fontStyle_1 = tkFont.Font(family="Lucida Grande", size=12)
     weiter = False
     ctr = 0
     print("commisioning wizard aufgerufen")
     para = Toplevel()
-    para.title('comissioning wizard')
+    para.title('task assistant')
     para.geometry('800x500')
     # Seitenüberschrift
     ScreenTitle = Label(master=para,
-                        text='Commisioning Wizard')
+                        text='Task Assistant')
     ScreenTitle.place(x=0, y=110, width=800, height=50)
 
-    #info = "project:  " + param.status["project"]
-    ProjectTitle = Label(master=para, text=info, font=fontStyle_1, anchor=W, justify=LEFT)
+    info = "project:  " + param.status["project"]
+    ProjectTitle = Label(master=para, text=info , anchor=W, justify=LEFT)
     ProjectTitle.place(x=10, y=20, width=800, height=20)
     info = "user:  " + param.status["user"]
-    ProjectUser = Label(master=para, text=info, font=fontStyle_1, anchor=W, justify=LEFT)
+    ProjectUser = Label(master=para, text=info,  anchor=W, justify=LEFT)
     ProjectUser.place(x=10, y=40, width=800, height=20)
     info = "date:  " + param.status["date/time"]
-    ProjectDate = Label(master=para, text=info, font=fontStyle_1, anchor=W, justify=LEFT)
+    ProjectDate = Label(master=para, text=info, anchor=W, justify=LEFT)
     ProjectDate.place(x=600, y=20, width=800, height=20)
 
     # TODO: ueberlegen, wie man das mit Schleifen (iterativ) hinbekommen könnte
@@ -111,11 +162,11 @@ def Wizard():
     ButtonNext = Button(master=para, text='Function OK', command=next)
     ButtonNext.place(x=400, y=320, width=150, height=20)
     weiter = True
-    for aktuell in params:
-        if weiter == True:
-            print("param.status ", " ", aktuell, param.status[aktuell])
-
-            weiter = False
+    #for aktuell in params:
+       # if weiter == True:
+       #     print("param.status ", " ", aktuell, param.status[aktuell])
+#
+         #   weiter = False
 
     # eintragen in Datenbank
 
